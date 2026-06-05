1f242afd34ebabe6fb764925c82b5a61507d8595
AinaTino <andriamananatino@gmail.com>
2026-06-05 23:22:21 +0300

    refactor(router): Update homepage path and auth redirect

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,15 +1,15 @@
 import "package:go_router/go_router.dart";
-import "package:materelia/features/homepage.dart";
+import "package:materelia/features/home/homepage.dart";
 import "package:materelia/shared/services/supabase_service.dart";
 
 final rootRouter = GoRouter(
   redirect:(context, state) {
     final isLoggedIn = SupabaseService.currentUser != null;
     final objectif = state.uri.path;
-    final allowedBefore = (objectif == '/signin') || (objectif == '/' ) || (objectif == '/signup') ;
+    final allowedBefore = (objectif == '/signin') || (objectif == '/signup') ;
 
     if (!isLoggedIn && !allowedBefore) {
-      return '/';
+      return '/signin';
     }
 
     if (isLoggedIn && allowedBefore){
