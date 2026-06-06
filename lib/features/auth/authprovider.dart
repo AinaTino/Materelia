0c6d25cab218152999f92659e8d6ead267f9a6d7
AinaTino <andriamananatino@gmail.com>
2026-06-06 13:09:38 +0300

    feat(auth): add AuthController and authService provider using Riverpod

---
+++ lib/features/auth/authprovider.dart
@@ -0,0 +1,51 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'auth_service.dart';
+
+part 'authprovider.g.dart';
+
+@riverpod
+AuthService authService(Ref ref) {
+  return AuthService();
+}
+
+@riverpod
+class AuthController extends _$AuthController {
+  @override
+  FutureOr<void> build() {}
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      await ref.read(authServiceProvider).signIn(
+        email: email,
+        password: password,
+      );
+    });
+  }
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required String nom,
+    required String prenom,
+  }) async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      await ref.read(authServiceProvider).signUp(
+        email: email,
+        password: password,
+        nom: nom,
+        prenom: prenom,
+      );
+    });
+  }
+
+  Future<void> signOut() async {
+    await ref.read(authServiceProvider).signOut();
+  }
+}
