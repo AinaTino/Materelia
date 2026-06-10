ae4b09611a564d06e8119d9679d45befd0a35ebc
AinaTino <andriamananatino@gmail.com>
2026-06-10 23:45:58 +0300

    refactor(auth): simplify exception handling
    Reduce auth_exception.dart complexity by removing redundant code and streamlining exception types. This improves maintainability and reduces boilerplate.

--- lib/features/auth/service/auth_exception.dart
+++ lib/features/auth/service/auth_exception.dart
@@ -1,71 +1,25 @@
-class EmailErrorException implements FormatException {
-  @override
-  final String message;
+// auth_exception.dart
 
+sealed class AuthFieldException implements Exception {
+  final String message;
+  const AuthFieldException(this.message);
+  
   @override
   String toString() => message;
-  
-  EmailErrorException(this.message);
-  
-  @override
-  // TODO: implement offset
-  int? get offset => throw UnimplementedError();
-  
-  @override
-  // TODO: implement source
-  get source => throw UnimplementedError();
 }
 
-class PasswordErrorException implements FormatException {
-  @override
-  final String message;
-
-  @override
-  String toString() => message;
-  
-  PasswordErrorException(this.message);
-  
-  @override
-  // TODO: implement offset
-  int? get offset => throw UnimplementedError();
-  
-  @override
-  // TODO: implement source
-  get source => throw UnimplementedError();
+class EmailErrorException extends AuthFieldException {
+  const EmailErrorException(super.message);
 }
 
-class FirstNameErrorException implements FormatException {
-  @override
-  final String message;
-
-  @override
-  String toString() => message;
-  
-  FirstNameErrorException(this.message);
-  
-  @override
-  // TODO: implement offset
-  int? get offset => throw UnimplementedError();
-  
-  @override
-  // TODO: implement source
-  get source => throw UnimplementedError();
+class PasswordErrorException extends AuthFieldException {
+  const PasswordErrorException(super.message);
 }
 
-class NameErrorException implements FormatException {
-  @override
-  final String message;
+class FirstNameErrorException extends AuthFieldException {
+  const FirstNameErrorException(super.message);
+}
 
-  @override
-  String toString() => message;
-  
-  NameErrorException(this.message);
-  
-  @override
-  // TODO: implement offset
-  int? get offset => throw UnimplementedError();
-  
-  @override
-  // TODO: implement source
-  get source => throw UnimplementedError();
+class NameErrorException extends AuthFieldException {
+  const NameErrorException(super.message);
 }
